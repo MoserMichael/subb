7df7172504006f2356d1cb631831c50b0ea99c9f
Michael Moser <moser.michael@gmail.com>
2021-11-01 13:08:02 +0200

    add pip build

---
+++ build-pip.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -ex
+
+# remove files not under git
+git clean -f -d
+
+mkdir -p staging_dir/src/subby
+mkdir -p staging_dir/tests
+
+cp test.py staging_dir/tests
+cp subby/*.py staging_dir/src/subby
+cp requirements.txt staging_dir/
+cp LICENSE.txt README.md staging_dir
+cp pip-build/* staging_dir
+
+if [[ -f build.zip ]]; then
+    rm build.zip
+fi
+
+#pushd staging_dir
+#zip  ../build.zip -r .
+#popd
+#
+#echo "*** files in build.zip ***"
+#unzip -l ./build.zip
+
+pushd staging_dir
+python3 setup.py sdist bdist_wheel
+
+python3 -m pip install --user --upgrade twine
+
+# twine is put here right now, so add it to path.
+export PATH=$HOME/Library/Python/3.9/bin:$PATH
+
+twine check dist/*
+
+cat <<EOF
+*** upload ***
+enter user: __token__
+for password: <pypi api token>
+EOF
+
+python3 -m twine upload --verbose dist/*
+
+popd
+
+
+echo "*** build completed ***"
+
---
+++ pip-build/setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8") as file:
+        return file.read()
+
+install_requires = read("requirements.txt")
+install_requires = install_requires.split("\n")
+install_requires = list(filter(lambda x: x[0:1] != "#", install_requires))
+print("install-requires:", install_requires)
+
+setuptools.setup(
+    name = "subby",
+    version = "1.0.0",
+    author = "Michael Moser",
+    author_email = "moser.michael@gmail.com",
+    description = ("subby is a wrapper for python subprocess module"),
+    license = "MIT",
+    keywords = "utility",
+    url = "https://github.com/MoserMichael/subby",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages("src"),
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing"
+    ],
+    python_requires='>=3.6',
+)
